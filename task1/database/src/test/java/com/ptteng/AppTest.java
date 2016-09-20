35d20f24e1b1f591ad1b99d7c62e3e7eae0e2c48
lzhang <lzhang@dmo.lzhang>
2016-09-20 16:59:20 +0800

    add log4j to project

--- task1/database/src/test/java/com/ptteng/AppTest.java
+++ task1/database/src/test/java/com/ptteng/AppTest.java
@@ -1,38 +1,12 @@
 package com.ptteng;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import com.ptteng.model.User;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+  @Test
+  public void test(){
+    System.out.println(User.class);
+  }
 }
